1e41fe6e1114c4735d7cce80bb72fbd15b2bcc48
ZGrahamz <graham_zach@outlook.com>
2024-06-03 23:30:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is even.");
+        }
+        else {
+            System.out.println(numToExamine + " is odd.");
+        }
+    }
+}
